db73ddc38b9855d907ff490f79b4c7ea181541a2
Francary <francarymolinapaez@gmail.com>
2023-12-05 21:25:59 -0300

    Mejora de las Clases del Home Page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ const HomePage = () => {
 
     return(
       
-        <div className="d-flex flex-column text-center m-5">
+        <div className="container d-flex flex-column p-3 gap-4 justify-content-center mt-5 text-center">
         <h1>HOME PAGE</h1>
         <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat accusantium eligendi doloribus? Quia mollitia non iure, atque id repellat, quis saepe autem odit aliquid, ea debitis dignissimos corporis commodi voluptates!</p>
         </div>
